c5845cccecca8f2e8f57a4bb8b9ef92b7493ffd0
Camilla Pinto <camilla.pintolisboa@icloud.com>
2024-05-20 20:42:37 -0300

    URLS e GET functions

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Person, Bill
+from .serializers import PersonSerializer, BillSerializer
+
+import json
+
+@api_view(['GET'])
+def get_person(request):
+    if request.method == 'GET':
+        person = Person.objects.all()
+
+        serializer = PersonSerializer(person, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+def get_bill(request):
+    if request.method == 'GET':
+        bill = Bill.objects.all()
+
+        serializer = BillSerializer(bill, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
