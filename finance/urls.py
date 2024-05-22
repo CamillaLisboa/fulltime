cd01039bbdb83ef0bc644d01dbb2c49748880df0
Camilla Pinto <camilla.pintolisboa@icloud.com>
2024-05-22 15:44:07 -0300

    view, url for post and put

--- finance/urls.py
+++ finance/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.get_person, name='get_all_person'),
+    path('persons', views.get_person, name='get_all_person'),
+    path('bills', views.get_bill, name='get_all_bill'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -10,19 +10,53 @@ from .serializers import PersonSerializer, BillSerializer
 
 import json
 
-@api_view(['GET'])
+@api_view(['GET','POST','PUT'])
 def get_person(request):
     if request.method == 'GET':
-        person = Person.objects.all()
+        try:
+            person = Person.objects.all()
 
-        serializer = PersonSerializer(person, many=True)
-        return Response(serializer.data)
+            serializer = PersonSerializer(person, many=True)
+            return Response(serializer.data)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'POST':
+
+        new_person = request.data
+
+        serializer = PersonSerializer(data = new_person)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    if request.method == 'PUT':
+        id = request.data['id']
+        updated_person = Person.objects.get(id=id)
+
+        serializer = PersonSerializer(updated_person, data=request.data)
     return Response(status=status.HTTP_404_NOT_FOUND)
-
+   
+   
+@api_view(['GET','POST','PUT'])
 def get_bill(request):
     if request.method == 'GET':
         bill = Bill.objects.all()
 
         serializer = BillSerializer(bill, many=True)
         return Response(serializer.data)
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'POST':
+
+        new_bill = request.data
+
+        serializer = BillSerializer(data = new_bill)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_404_NOT_FOUND)
+    
+
+
